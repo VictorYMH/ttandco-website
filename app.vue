0124d0f59a5e29bde2e969310734ae676e1166c2
victor <ymhvic@gamil.com>
2025-03-22 22:39:39 +0800

    add pages

--- app.vue
+++ app.vue
@@ -1,6 +1,21 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div id="app">
+    <NuxtPage />
   </div>
 </template>
+
+<script>
+export default {
+  // You can add global settings or methods here
+}
+</script>
+
+<style>
+/* Add global styles here */
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <img src="/main-logo.png" class="logo" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  height: 100vh;
+  overflow: hidden;
+}
+
+.logo {
+  max-width: 100%;
+  max-height: 100%;
+}
+</style>

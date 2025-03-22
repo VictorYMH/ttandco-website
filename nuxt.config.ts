1ea78cdc7413216cc20afa3ef683d7f594a3f096
victor <ymhvic@gamil.com>
2025-03-22 23:24:37 +0800

    update meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  app: {
+    head: {
+      title: 'TT&CO China', // default fallback title
+      htmlAttrs: {
+        lang: 'cn',
+      },
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ]
+    }
+  },
+
+  devtools: { enabled: true },
+  compatibilityDate: '2025-03-22'
 })

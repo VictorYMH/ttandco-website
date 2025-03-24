17acb740e3e77115cbd3662259c316c6851a9892
Victor <ymhvic@gamil.com>
2025-03-24 14:22:32 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,9 @@ SERVICE_NAME="ttandco_website"
 SERVICE_FILE="/etc/systemd/system/$SERVICE_NAME.service"
 NODE_APP_PATH="/usr/ttandco-website/.output/server/index.mjs"
 
+# Stop the service if it is running
+sudo systemctl stop $SERVICE_NAME
+
 # Remove the existing service file if it exists
 if [ -f "$SERVICE_FILE" ]; then
     sudo rm "$SERVICE_FILE"
@@ -12,16 +15,15 @@ fi
 # Create the systemd service file
 sudo bash -c "cat > $SERVICE_FILE" <<EOL
 [Unit]
-Description=Node.js Application
+Description=ttandco web
 After=network.target
 
 [Service]
-Environment=PORT=80
-ExecStart=/usr/bin/node $NODE_APP_PATH
+ExecStart=/root/.nvm/versions/node/v22.14.0/bin/node $NODE_APP_PATH
 Restart=always
 RestartSec=10
-User=nobody
-Group=nogroup
+User=root
+Group=root
 StandardOutput=syslog
 StandardError=syslog
 SyslogIdentifier=$SERVICE_NAME
